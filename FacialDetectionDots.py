60734df7b5427c71b31dce6040f52796054acc80
OutdoorScientist <78631035+OutdoorScientist@users.noreply.github.com>
2022-04-21 17:43:22 -0500

    working camera rotation

--- FacialDetectionDots.py
+++ FacialDetectionDots.py
@@ -1,5 +1,6 @@
 #source: https://machinelearningmastery.com/how-to-perform-face-detection-with-classical-and-deep-learning-methods-in-python-with-keras/
 # face detection with mtcnn on a photograph
+
 from matplotlib import pyplot
 from matplotlib.patches import Rectangle
 from matplotlib.patches import Circle
@@ -7,7 +8,10 @@ from mtcnn.mtcnn import MTCNN
 
 import cv2
 from numpy import asarray
-from PIL import Image
+## for brightness
+import math
+from PIL import Image, ImageStat
+##
 
 # draw an image with detected objects
 def draw_image_with_boxes(filename, result_list):
@@ -35,13 +39,12 @@ def draw_image_with_boxes(filename, result_list):
 
 # filename = 'color_img02.JPG'
 
-def detect_face_pos(filename):
+def detect_face_pos(filename,context_dic):
     pixels = pyplot.imread(filename)
     # create the detector, using default weights
     detector = MTCNN()
     # detect faces in the image
     faces = detector.detect_faces(pixels)
-    context_dic ={}
 
     if len(faces)==0:
         #no face detected
@@ -96,15 +99,68 @@ def detect_face_pos(filename):
             print('Move camera down')
             context_dic["down"]= "Recommendation - Move camera down"
             #return ('Recommendation - Move camera down')
-        if len(context_dic) ==0:
-            print("picture good")
-            context_dic["good"] = "Good"
-            #return('Good')
+
+        context_dic = camera_rotation(faces,context_dic)
+
         return context_dic
         # draw_image_with_boxes(filename, faces)
         # quit()
     print("Got to far Nathaniel **Check this")
+
+def brightness( im_file , context_dic):
+    im = Image.open(im_file)
+    stat = ImageStat.Stat(im)
+    r,g,b = stat.mean
+    brightness = math.sqrt(0.241*(r**2) + 0.691*(g**2) + 0.068*(b**2))
+    print("brightness:",brightness)
+    if int(brightness) > 130:
+        context_dic["brightness"] = "Recommendation - Make Environment Darker"
+    if int(brightness) < 60:
+        print("int brightness:",int(brightness))
+        print("been determined its to dark")
+        context_dic["brightness"] = "Recommendation - Make Environment Brighter"
+    else:
+        pass
+    return context_dic
+
+def camera_rotation(faces,context_dic):
+
+    l_x = faces[0]['keypoints']['left_eye'][0]
+    l_y = faces[0]['keypoints']['left_eye'][1]
+    r_x = faces[0]['keypoints']['right_eye'][0]
+    r_y = faces[0]['keypoints']['right_eye'][1]
+
+    degree_of_rotation = math.degrees(math.atan(abs((r_y - l_y)) / (r_x - l_x)))
+    print("degree_of_rotation",degree_of_rotation)
+    # if abs((l_y - r_y)) > 30:
+    if degree_of_rotation > 25:
+        if (r_y > l_y):
+            print("degree_of_rotation", degree_of_rotation)
+            print("counter clockwise")
+            context_dic["rotate"] = "Recommendation - Rotate your camera clockwise"
+        if (l_y > r_y):
+            print("degree_of_rotation", degree_of_rotation)
+            print("rotate clockwise")
+            context_dic["rotate"] = "Recommendation - Rotate your camera counter-clockwise"
+
+    return context_dic
+
+def analyzePhoto(file):
+    context_dic = {}
+    context_dic = detect_face_pos(file,context_dic)
+    print("context_dic:",context_dic)
+    if "noface" in context_dic:
+        return context_dic
+    elif "faces" in context_dic:
+        return context_dic
+    else:
+        brightness(file, context_dic)
+
+    if len(context_dic) == 0:
+        print("picture good")
+        context_dic["good"] = "Good"
+    return context_dic
+
 # to do
-# - Check for orientation of head
-# - Check for center of head in camera view
-# - chek for brightness
+#     - need to get detected more than one face
+#       - great camera feed.
